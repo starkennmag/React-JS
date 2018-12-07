c6d79a7384661d8408b0bcc026c5cfe2e882ba75
Nelson Gomes <nelson.gomes@brandit.pt>
2018-12-07 17:43:10 +0000

    aplicação com metodos add e delete no dom

---
+++ src/Ninjas.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Ninjas = ({ninjas, deleteNinja}) =>{
+        //console.log(this.props);
+        //const { ninjas } = props;
+        // const ninjaList = ninjas.map(ninja => {
+        //     if(ninja.age > 20){
+        //     return(
+        //     <div className="ninja" key={ninja.id}>
+        //         <div>Name: {ninja.name}</div>
+        //         <div>Age: {ninja.age}</div>
+        //         <div>Belt: {ninja.belt}</div>
+        //     </div>
+        //     )
+        //     } else{
+        //         return null
+        //     }
+        // })
+        const ninjaList = ninjas.map(ninja => {
+            //condition ? () : ()
+            return ninja.age > 20 ? (
+                <div className="ninja" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                    <button onClick={ () => {deleteNinja(ninja.id)}}>Delete Ninja</button>
+            </div>
+            ) : null;
+        })
+        return(
+            <div className="ninja-list">
+                { ninjaList }
+            </div>
+        )
+}
+
+export default Ninjas;

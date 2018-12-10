5a57f747ac4020bf27a40d075013a1ea0b9acb7f
Nelson Gomes <nelson.gomes@brandit.pt>
2018-12-10 18:01:42 +0000

    posts e paginas com routing a funcionar

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa illo corrupti omnis vero in iste quaerat saepe? Officia mollitia molestiae laudantium voluptatem dolorum, ipsum itaque ratione, minima minus pariatur architecto?</p>
+        </div>
+    )
+}
+
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Contact = (props) => {
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000);
+    return (
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa illo corrupti omnis vero in iste quaerat saepe? Officia mollitia molestiae laudantium voluptatem dolorum, ipsum itaque ratione, minima minus pariatur architecto?</p>
+        </div>
+    )
+}
+
+export default Contact
